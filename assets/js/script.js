dec1a3054559eabcaae18d9c2d50c6ef16ec83f2
Keevin Aroco <gavikde2022@gmail.com>
2025-01-20 13:28:17 +0000

    create function to display exceptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@ document.getElementById("submit").addEventListener("click", e => postForm(e));
 function processOptions(form) {
     let optArray = [];
     
-    for (let entry of form.entries()) {
-        if (entry[0] === "options") {
-            optArray.push(entry[1]);
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
         }
     }
     form.delete("options");
@@ -36,8 +36,9 @@ async function postForm(e) {
         if (response.ok) {
             displayErrors(data);
         } else {
+            displayException(data);
             throw new Error(data.error);
-        }
+        };
 }
 
 function displayErrors(data) {
@@ -52,8 +53,8 @@ function displayErrors(data) {
             results += `<div>At line <span class="line">${error.line}</span>, `;
             results += `column <span class="column">${error.col}</span></div>`;
             results += `<div class="error">${error.error}</div>`;
-        }
-    }
+        };
+    };
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
@@ -71,6 +72,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     };
 }
@@ -80,6 +82,20 @@ function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until"</div>`
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
+
+function displayException(data) {
+
+    let heading = `An Exception occured`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
